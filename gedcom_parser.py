a7f3707ef0afe9ef4461672349c40930ee254295
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-09-24 23:42:38 -0400

    updated for Project 3

--- gedcom_parser.py
+++ gedcom_parser.py
@@ -1,8 +1,18 @@
+import pandas as pd
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from datetime import date
+
+individuals = []
+individuals_id_and_name = []
+families = []
+
 allowed_tags = {
     0 : ['INDI','FAM','HEAD','TRLR','NOTE'],
     1 : ['NAME','SEX','BIRT','DEAT','FAMC','FAMS','MARR','HUSB','WIFE','CHIL','DIV'],
     2 : ['DATE']
 }
+
 
 def parse_gedcom(filename):
   f = open(filename, "r")
@@ -29,3 +39,143 @@ def parse_gedcom(filename):
     print('--> ' + line)
     print('<-- ' + level + '|' + tag + 
           '|' + valid + '|' + arguments)
+  
+
+def save_ind_data(filename):
+  individuals = []
+  individuals_id_and_name = {}
+  f = open(filename, "r")
+  lines = f.readlines()
+
+  indices = []
+  for i in range(0,len(lines)):
+    line = lines[i]
+    level = line[0]
+    if 'INDI' in line.split() and level == '0':
+      indices.append(i)
+
+  for i in indices:
+    is_dead = False
+    person = {
+    }
+    if indices.index(i) != len(indices)-1:
+      for j in range(i,indices[indices.index(i)+1]):
+        line = lines[j]
+        level = line[0]
+        if 'INDI' in line.split() and level == '0':
+          person['id'] = line.split()[1].replace('@','')
+        if 'NAME' in line.split():
+          person['name'] = line.split('NAME')[1].replace('\n','')
+          individuals_id_and_name[person['id']] = person['name']
+        if 'SEX' in line.split() and level == '1':
+          person['gender'] = line.split()[2]
+        if 'BIRT' in line.split():
+          person['birthday'] =  lines[j+1].split('DATE')[1].replace('\n','')
+        if 'DEAT' in line.split():
+          is_dead = True
+          person['death'] = lines[j+1].split('DATE')[1].replace('\n','')
+        if 'FAMC' in line.split():
+          person['child'] = line.split()[2].replace('@','')
+        if 'FAMS' in line.split():
+          person['spouse'] = line.split()[2].replace('@','')
+    else:
+      for j in range(i,len(lines)-1):
+        line = lines[j]
+        level = line[0]
+        if 'INDI' in line.split() and level == '0':
+          person['id'] = line.split()[1].replace('@','')
+        if 'NAME' in line.split():
+          person['name'] = line.split('NAME')[1].replace('\n','')
+          individuals_id_and_name[person['id']] = person['name']
+        if 'SEX' in line.split() and level == '1':
+          person['gender'] = line.split()[2]
+        if 'BIRT' in line.split():
+          person['birthday'] = lines[j+1].split('DATE')[1].replace('\n','')
+        if 'DEAT' in line.split():
+          is_dead = True
+          person['death'] = lines[j+1].split('DATE')[1].replace('\n','')
+        if 'FAMC' in line.split():
+          person['child'] = line.split()[2].replace('@','')
+        if 'FAMS' in line.split():
+          person['spouse'] = line.split()[2].replace('@','')
+
+    if is_dead:
+       person['age'] = relativedelta(pd.to_datetime(person['death'],infer_datetime_format=True), pd.to_datetime(person['birthday'],infer_datetime_format=True)).years
+    else:
+      today = date.today()
+      person['age'] = relativedelta(today, pd.to_datetime(person['birthday'],infer_datetime_format=True)).years
+
+
+    individuals.append(person)
+
+      
+  return (individuals,individuals_id_and_name)
+
+def save_family_data(filename, individuals_id_and_name):
+  families = []
+  f = open(filename, "r")
+  lines = f.readlines()
+
+  indices = []
+  for i in range(0,len(lines)):
+    line = lines[i]
+    level = line[0]
+    if 'FAM' in line.split() and level == '0':
+      indices.append(i)
+  
+  for i in indices:
+    family = {
+    }
+    if indices.index(i) != len(indices)-1:
+      for j in range(i,indices[indices.index(i)+1]):
+        line = lines[j]
+        level = line[0]
+        if 'FAM' in line.split() and level == '0':
+          family['id'] = line.split()[1].replace('@','')
+        if 'MARR' in line.split():
+          family['married'] = lines[j+1].split('DATE')[1].replace('\n','')
+        if 'DIV' in line.split():
+          family['divorced'] = lines[j+1].split('DATE')[1].replace('\n','')
+        if 'HUSB' in line.split():
+          family['Husband ID'] = line.split()[2].replace('@','')
+          family['Husband Name'] = individuals_id_and_name[family['Husband ID']]
+        if 'WIFE' in line.split():
+          family['Wife ID'] = line.split()[2].replace('@','')
+          family['Wife Name'] = individuals_id_and_name[family['Wife ID']]
+        if 'CHIL' in line.split():
+          if 'children' in family: 
+            family['children'].append(line.split()[2].replace('@',''))
+          else:
+            family['children'] = []
+            family['children'].append(line.split()[2].replace('@',''))
+    else:
+      for j in range(i,len(lines)-1):
+        line = lines[j]
+        level = line[0]
+        if 'FAM' in line.split() and level == '0':
+          family['id'] = line.split()[1].replace('@','')
+        if 'MARR' in line.split():
+          family['married'] = lines[j+1].split('DATE')[1].replace('\n','')
+        if 'DIV' in line.split():
+          family['divorced'] = lines[j+1].split('DATE')[1].replace('\n','')
+        if 'HUSB' in line.split():
+          family['Husband ID'] = line.split()[2].replace('@','')
+          family['Husband Name'] = individuals_id_and_name[family['Husband ID']]
+        if 'WIFE' in line.split():
+          family['Wife ID'] = line.split()[2].replace('@','')
+          family['Wife Name'] = individuals_id_and_name[family['Wife ID']]
+        if 'CHIL' in line.split():
+          if 'children' in family: 
+            family['children'].append(line.split()[2].replace('@',''))
+          else:
+            family['children'] = []
+            family['children'].append(line.split()[2].replace('@',''))
+
+    families.append(family)
+
+  return families
+
+(individuals,individuals_id_and_name) = save_ind_data("/content/GedcomFamily-SharattramSeenivasan.ged")
+display(pd.DataFrame(individuals))
+families = save_family_data("/content/GedcomFamily-SharattramSeenivasan.ged", individuals_id_and_name)
+display(pd.DataFrame(families))
